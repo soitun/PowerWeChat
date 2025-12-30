fe8affacfb5b0d20ffe640718a35cde03b142d40
Matrix-X <matrix-x@aritsan-cloud.com>
2025-12-30 15:19:13 +0800

    fix(openPlatform): change ResponseSubmitAudit audit id type

--- src/openPlatform/authorizer/miniProgram/code/response/responseCode.go
+++ src/openPlatform/authorizer/miniProgram/code/response/responseCode.go
@@ -28,7 +28,7 @@ type ResponseSubmitAudit struct {
 
 	Type    string `json:"type"`
 	MediaID string `json:"mediaid"`
-	AuditID string `json:"auditid"`
+	AuditID int    `json:"auditid"`
 }
 
 type ResponseGetAuditStatus struct {
