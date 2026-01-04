0fc6df1f50bda2a46390cdde6e601b4b364fff52
Matrix-X <matrix-x@aritsan-cloud.com>
2026-01-04 21:53:21 +0800

    update(openplatform): miniprogram privacy get with new response struct

--- src/openPlatform/authorizer/miniProgram/privacy/response/responsePrivacy.go
+++ src/openPlatform/authorizer/miniProgram/privacy/response/responsePrivacy.go
@@ -2,19 +2,53 @@ package response
 
 import "github.com/ArtisanCloud/PowerWeChat/v3/src/kernel/response"
 
-type Interface struct {
-	ApiName   string `json:"api_name"`
-	ApiChName string `json:"api_ch_name"`
-	ApiDesc   string `json:"api_desc"`
-	Status    int    `json:"status"`
-	ApiLink   string `json:"api_link"`
-	GroupName string `json:"group_name"`
+type Setting struct {
+	PrivacyKey   string `json:"privacy_key"`
+	PrivacyText  string `json:"privacy_text"`
+	PrivacyLabel string `json:"privacy_label"`
+}
+
+type OwnerSetting struct {
+	ContactPhone         string `json:"contact_phone"`
+	ContactEmail         string `json:"contact_email"`
+	ContactQq            string `json:"contact_qq"`
+	ContactWeixin        string `json:"contact_weixin"`
+	StoreExpireTimestamp string `json:"store_expire_timestamp"`
+	ExtFileMediaId       string `json:"ext_file_media_id"`
+	NoticeMethod         string `json:"notice_method"`
+}
+
+type Desc struct {
+	PrivacyKey  string `json:"privacy_key"`
+	PrivacyDesc string `json:"privacy_desc"`
+}
+
+type PrivacyDesc struct {
+	PrivacyDescList []Desc `json:"privacy_desc_list"`
+}
+
+type Sdk struct {
+	PrivacyKey   string `json:"privacy_key"`
+	PrivacyText  string `json:"privacy_text"`
+	PrivacyLabel string `json:"privacy_label"`
+}
+
+type SdkPrivacyInfo struct {
+	SdkName    string `json:"sdk_name"`
+	SdkBizName string `json:"sdk_biz_name"`
+	SdkList    []Sdk  `json:"sdk_list"`
 }
 
 type ResponseGet struct {
 	response.ResponseOpenPlatform
 
-	InterfaceList []*Interface `json:"interface_list"`
+	CodeExist          int              `json:"code_exist"`
+	PrivacyList        []string         `json:"privacy_list"`
+	SettingList        []Setting        `json:"setting_list"`
+	UpdateTime         int              `json:"update_time"`
+	OwnerSetting       OwnerSetting     `json:"owner_setting"`
+	PrivacyDesc        PrivacyDesc      `json:"privacy_desc"`
+	SdkPrivacyInfoList []SdkPrivacyInfo `json:"sdk_privacy_info_list"`
 }
 
 type ResponseUpload struct {
