d519fcb46c0f6db90ad52a18ef283a0d33914008
Matrix-X <matrix-x@aritsan-cloud.com>
2026-03-17 14:26:11 +0800

    fix(work): change ResponseGroupChatOpenGIDToChatID ChatID type

--- src/work/externalContact/groupChat/response/responseGroupChatOpenGIDToChatID.go
+++ src/work/externalContact/groupChat/response/responseGroupChatOpenGIDToChatID.go
@@ -1,11 +1,10 @@
 package response
 
 import (
-	"github.com/ArtisanCloud/PowerWeChat/v3/src/kernel/power"
 	"github.com/ArtisanCloud/PowerWeChat/v3/src/kernel/response"
 )
 
 type ResponseGroupChatOpenGIDToChatID struct {
 	response.ResponseWork
-	ChatID *power.HashMap `json:"chat_id"`
+	ChatID string `json:"chat_id"`
 }
